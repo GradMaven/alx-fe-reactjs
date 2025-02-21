877bdbe91a495556a19df230459660857ab39bcc
Leonard Ondigo <109470807+GradMaven@users.noreply.github.com>
2025-02-21 10:44:09 -0400

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,7 +2,7 @@ function MainContent() {
     return (
         <main style={{ padding: '20px', textAlign: 'center', backgroundColor: '#f4f4f4' }}>
       <p style={{ fontSize: '18px', color: '#333' }}>
-        Explore some of the best cities in the world with amazing culture and landscapes!
+        I love to visit New York, Paris, and Tokyo.
       </p>
     </main>
     );
