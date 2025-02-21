32409addbef15464ee5e20d8263188851b8b2fe5
Leonard Ondigo <109470807+GradMaven@users.noreply.github.com>
2025-02-21 11:32:10 -0400

    Add files via upload

---
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -0,0 +1,14 @@
+// ProfilePage.jsx
+import React from 'react';
+import UserInfo from '../UserInfo';
+
+function ProfilePage() {
+  return (
+    <div>
+       <h1>User Profile Card</h1>
+      <UserInfo />
+    </div>
+  );
+}
+
+export default ProfilePage;
