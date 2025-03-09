0feff5e6fcdc9ec141703ecd5e00a6755ef5fc44
Leonard Ondigo <109470807+GradMaven@users.noreply.github.com>
2025-03-09 06:58:28 -0400

    Update ProtectedRoute.jsx

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,8 +1,12 @@
+
 import { Navigate } from "react-router-dom";
-import Auth from "../auth";
+import { useAuth } from "../hooks/useAuth";
 
 const ProtectedRoute = ({ children }) => {
-  return Auth.isAuthenticated ? children : <Navigate to="/" />;
+  const { isAuthenticated } = useAuth();
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
+
