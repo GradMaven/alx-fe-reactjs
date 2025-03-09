955c839acdc3f83148abd466ebca763cad772abd
Leonard Ondigo <109470807+GradMaven@users.noreply.github.com>
2025-03-09 08:18:06 -0400

    Update App.jsx

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import TodoList from "./components/TodoList";
-import AddTodoForm from "./components/AddTodoForm"
 
 function App() {
   return (
     <div>
       <h1>React Todo App</h1>
       <TodoList />
-      <AddTodoForm />
     </div>
   );
 }
