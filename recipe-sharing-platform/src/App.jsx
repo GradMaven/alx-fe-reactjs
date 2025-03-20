2f764fa91ba87fc0c0c2bfee244fc83f1f397a27
Leonard Ondigo <109470807+GradMaven@users.noreply.github.com>
2025-03-20 11:39:55 -0400

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+
 import './App.css'
 import HomePage from './components/HomePage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RecipeDetail from './components/RecipeDetail';
+import AddRecipeForm from './components/AddRecipeForm';
 
 function App() {
  
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/recipe/:id" element={<RecipeDetail />} />
+        <Route path="/recipe/form" element ={<AddRecipeForm />} />
       </Routes>
     </Router>
      
