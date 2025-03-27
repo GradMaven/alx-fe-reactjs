f0f02d671c11dd541231f82de7d8101001df0c03
Leonard Ondigo <109470807+GradMaven@users.noreply.github.com>
2025-03-27 11:52:26 -0400

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,16 +1,14 @@
 
-
 import axios from 'axios';
 
 const API_URL = 'https://api.github.com/users/';
 const API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY; 
 
-export const fetchGitHubUser = async (username) => {
+
+export const fetchUserData = async (username) => {
   try {
     const response = await axios.get(`${API_URL}${username}`, {
-      headers: {
-        Authorization: `token ${API_KEY}` 
-      }
+      headers: API_KEY ? { Authorization: `token ${API_KEY}` } : {},
     });
     return response.data;
   } catch (error) {
@@ -18,3 +16,4 @@ export const fetchGitHubUser = async (username) => {
     return null;
   }
 };
+
